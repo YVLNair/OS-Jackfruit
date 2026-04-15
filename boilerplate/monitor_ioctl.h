778fc78c08b651e6f6db785cc8ec5329915c7597
YVLNair <sarangnair69420@gmail.com>
2026-04-16 00:10:21 +0530

    Fixed ioctl header to match engine implementation

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -1,7 +1,16 @@
 #ifndef MONITOR_IOCTL_H
 #define MONITOR_IOCTL_H
 
-#define IOCTL_SET_LIMIT 1
-#define IOCTL_GET_USAGE 2
+#define CONTAINER_ID_LEN 32
+
+struct monitor_request {
+    int pid;
+    unsigned long soft_limit_bytes;
+    unsigned long hard_limit_bytes;
+    char container_id[CONTAINER_ID_LEN];
+};
+
+#define MONITOR_REGISTER 1
+#define MONITOR_UNREGISTER 2
 
 #endif
